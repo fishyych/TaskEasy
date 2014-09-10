e01dfdc333b4b2f3a3916f0ca20f3a1e8a0e1ab1
Cheney Yuan <ych_gem@126.com>
2014-09-10 21:01:37 +0800

    initial commit for task easy

---
+++ TaskEasy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TaskEasy
+//
+//  Created by 长海 袁 on 14-9-10.
+//  Copyright (c) 2014年 长海 袁. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
